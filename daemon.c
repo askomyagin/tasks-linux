b5ca34a80e8d8eb0faa683dc9ade89555c376e72
askomyagin <55476117+askomyagin@users.noreply.github.com>
2020-05-24 21:38:54 +0300

    second version

--- daemon.c
+++ daemon.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-#include <string.h>
 #include <sys/stat.h>
-#include <stdlib.h>
+#include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+sem_t semaphore;
 
 bool flag = false;
 bool flag2 = false;
+bool flag3 = false;
 
 void signal_hundler(int signum)
 {
@@ -21,6 +25,11 @@ void signal_hundler2(int signum)
 {
 	flag2 = true;
 }
+void SigChildHandler(int signum)
+{
+	flag3 = true;
+}
+
 
 int Command(char **argv)
 {
@@ -35,54 +44,94 @@ int Command(char **argv)
 
 int Demon(char **argv)
 {
-	pid_t pid;
-	char *argv1[20];
-	int file;
-	char *arg;	
+	sem_init(&semaphore, 0, 1);
+	pid_t pid;	
 	char buf[] = "Signal received\n";
 	char buf1[] = "Daemon finished\n";
+	char buf2[] = "semaphore wait\n";
 
 	signal(SIGUSR1, signal_hundler);
+	signal(SIGCHLD, SigChildHandler);
 	signal(SIGTERM, signal_hundler2);
 
-	char string[256];
-	int i = 0;
-	file = open(argv[1], O_CREAT | O_RDWR, 00755);
+	//----------------------------------------------------
+	char string[64256];
+	int file = open(argv[1], O_CREAT | O_RDWR, 00755);
 	read(file,string,sizeof(string));
-	
-	arg = strtok(string, " ");
-	argv1[i++] = arg;
-	
-	while(arg!=NULL)
-	{
-		arg = strtok(NULL, " ");
-		argv1[i++]=arg;			
-	}
 
-	argv1[i++] = NULL;
+	char *line;
+	int count=0;
+	char *mass_line[20];
+
+	line = strtok(string,"\n");
+	while(line!=NULL)
+	{
+		mass_line[count++] = line;
+		line = strtok(NULL,"\n");
+	}
+	//----------------------------------------------------
 
 	while (!flag2)
 	{
 		if (flag)
 		{
-			pid = fork();
+			for (int n=0; n<count;n++){
+				pid = fork();
 
-			if (pid == -1)
-			{
-				printf("Problem!\n");
-				exit(1);
+				if (pid == -1)
+				{
+					printf("Problem with fork!\n");
+				}
+				else if (pid == 0)
+				{
+					//-------------------------------------
+					char *argv1[20];
+					char *arg;	
+					int i = 0;
+
+					arg = strtok(mass_line[n], " ");
+					argv1[i++] = arg;
+
+					while(arg!=NULL)
+					{	
+						arg = strtok(NULL, " ");
+						argv1[i++]=arg;			
+					}
+
+					argv1[i] = NULL;
+
+					int wait = sem_wait(&semaphore);
+
+					if (wait < 0){
+						printf("%s",buf2);
+					}
+					else{
+						printf("command complited: %s", argv1[0]);
+						Command(argv1);
+					}
+					//----------------------------------------------
+				}
+				else if (pid > 0)
+					{
+						while (1)
+						{
+							if (flag3)
+							{
+								waitpid(-1, NULL, 0);
+								sem_post(&semaphore);
+								flag3 = false;
+								break;
+							}
+							pause();
+						}
+					}
+				flag = false;
 			}
-			else if (pid == 0)
-			{
-				Command(argv1);
-				printf("%s", buf);
-			}
-			flag = false;
 		}
-	
 		pause();
 		//sleep(50);
 	}
+	sem_destroy(&semaphore);
 	printf("%s", buf1);
 	return 0;
 }
